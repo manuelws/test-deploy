93a05eeda24d3a090cbca8759b621644539949b6
Manuel <manuel@webschuppen.com>
2024-05-28 15:30:10 +0200

    Add analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import "./App.css";
+import { Analytics } from "@vercel/analytics/react"
+
 
 function App() {
 
   return (
     <>
+      <Analytics />
       <h1 style={{fontSize: '2.5rem', paddingInline: '20px'}}>Chilling mit Finn und Tim</h1>
     </>
   );
